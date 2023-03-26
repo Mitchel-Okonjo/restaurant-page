ada921b11d0f61a21939521943267225e848860f
Mitchel-Okonjo <111641404+Mitchel-Okonjo@users.noreply.github.com>
2023-03-27 00:16:57 +0100

    Create Menu page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import { loadHome } from "./home";
+import { loadMenu } from "./menu";
 
-loadHome();
+loadMenu();
---
+++ src/menu.js
@@ -0,0 +1,79 @@
+import { createHeader } from "./home";
+
+function setPrice(amount) {
+  const price = document.createElement("div");
+  price.classList.add("dish-price");
+  price.textContent = `$${amount}`;
+
+  return price;
+}
+
+function setDishComp(components) {
+  const dishComp = document.createElement("div");
+  dishComp.classList.add("dish-comp");
+  dishComp.textContent = `${components}`;
+
+  return dishComp;
+}
+
+function setDishTitle(title) {
+  const dishTitle = document.createElement("div");
+  dishTitle.classList.add("dish-title");
+  dishTitle.textContent = `${title}`;
+
+  return dishTitle;
+}
+
+function setDishImage(url, alt) {
+  const dishImage = document.createElement("img");
+  dishImage.setAttribute("src", url);
+  dishImage.setAttribute("alt", alt);
+
+  return dishImage;
+}
+
+function createDish(url, alt, title, components, amount) {
+  const image = setDishImage(url, alt);
+  const dishTitle = setDishTitle(title);
+  const dishComp = setDishComp(components);
+  const price = setPrice(amount);
+
+  const dish = document.createElement("div");
+  dish.classList.add("dish");
+  dish.appendChild(image);
+  dish.appendChild(dishTitle);
+  dish.appendChild(dishComp);
+  dish.appendChild(price);
+
+  return dish;
+}
+
+function createMenu() {
+  const menu = document.createElement("div");
+  menu.classList.add("menu");
+
+  return menu;
+}
+
+function loadMenu() {
+  const content = document.querySelector("#content");
+  const header = createHeader();
+  content.appendChild(header);
+  const menu = createMenu();
+
+  for (let i = 0; i <= 5; i++) {
+    const dish = createDish(
+      "images/jollof-rice.jpeg",
+      "Jollof Rice",
+      "Jollof Rice",
+      "Jollof rice, chicken, fried plantains",
+      "20.00"
+    );
+
+    menu.appendChild(dish);
+  }
+
+  content.appendChild(menu);
+}
+
+export { loadMenu };
